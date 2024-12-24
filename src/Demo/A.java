67cf6b9e39b1ff8263e147609e0429373a88dda6
Siddartrh720 <siddarthavula12@gmail.com>
2024-12-24 10:50:08 +0530

    first commit

---
+++ src/Demo/A.java
@@ -0,0 +1,27 @@
+package Demo;
+
+public class A {
+			//5.68
+		public static void main(String[] args) {
+		
+			
+			float num = 5.1114f;
+			
+			num = (int)(num * 1000);
+			System.out.println(num);
+			// 5678
+			int r = (int)(num % 10);// 8
+			num = (int)(num / 10);
+			if(r > 5) {
+				// 5678
+				// 567+1---568---5.68
+				num++;
+			}
+			// 568--- 5.68
+			num = num / 100;
+			// 5.68
+			
+			System.out.println("num--->"+num);
+
+    }	
+}
